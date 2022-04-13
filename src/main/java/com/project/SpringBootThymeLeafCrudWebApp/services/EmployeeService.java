e67b09be3f4b99e7d8a1f92d74d5ba10220385be
david-kariuki <davidkariukikamau0@gmail.com>
2022-04-13 17:15:27 +0300

    Updated Employee Service

--- src/main/java/com/project/SpringBootThymeLeafCrudWebApp/services/EmployeeService.java
+++ src/main/java/com/project/SpringBootThymeLeafCrudWebApp/services/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.project.SpringBootThymeLeafCrudWebApp.services;
 
-import com.project.SpringBootThymeLeafCrudWebApp.model.Employee;
+import com.project.SpringBootThymeLeafCrudWebApp.models.Employee;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface EmployeeService {
 
     List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
 }
