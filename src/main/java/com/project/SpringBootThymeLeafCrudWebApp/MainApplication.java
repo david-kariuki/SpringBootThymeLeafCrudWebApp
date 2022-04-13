45d306dcafd84eab167d1a137b2c47ae933e770d
david-kariuki <davidkariukikamau0@gmail.com>
2022-04-13 17:17:25 +0300

    Added @RestController annotation

--- src/main/java/com/project/SpringBootThymeLeafCrudWebApp/MainApplication.java
+++ src/main/java/com/project/SpringBootThymeLeafCrudWebApp/MainApplication.java
@@ -2,7 +2,9 @@ package com.project.SpringBootThymeLeafCrudWebApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MainApplication {
 
