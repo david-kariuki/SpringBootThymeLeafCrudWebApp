99b190d6569b197f2fa17f08ee22788feccdca6a
david-kariuki <davidkariukikamau0@gmail.com>
2022-04-13 10:59:05 +0300

    Created Employee JPA Repository

---
+++ src/main/java/com/project/SpringBootThymeLeafCrudWebApp/model/Employee.java
@@ -0,0 +1,55 @@
+package com.project.SpringBootThymeLeafCrudWebApp.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_address")
+    private String emailAddress;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+}
