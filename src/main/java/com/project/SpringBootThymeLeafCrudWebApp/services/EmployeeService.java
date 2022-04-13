849b24b24b54cd31636541b87056c7498eb73651
david-kariuki <davidkariukikamau0@gmail.com>
2022-04-13 10:59:15 +0300

    Created Employee Service

---
+++ src/main/java/com/project/SpringBootThymeLeafCrudWebApp/services/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.project.SpringBootThymeLeafCrudWebApp.services;
+
+import com.project.SpringBootThymeLeafCrudWebApp.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+}
