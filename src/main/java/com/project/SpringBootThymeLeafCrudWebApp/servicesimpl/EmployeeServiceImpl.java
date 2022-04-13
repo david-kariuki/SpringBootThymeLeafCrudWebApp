4f9a83d97ddb284cfcc6cf945ae3d65b5b6c3f93
david-kariuki <davidkariukikamau0@gmail.com>
2022-04-13 17:15:38 +0300

    Updated Employee Service Implementation

--- src/main/java/com/project/SpringBootThymeLeafCrudWebApp/servicesimpl/EmployeeServiceImpl.java
+++ src/main/java/com/project/SpringBootThymeLeafCrudWebApp/servicesimpl/EmployeeServiceImpl.java
@@ -1,12 +1,14 @@
 package com.project.SpringBootThymeLeafCrudWebApp.servicesimpl;
 
-import com.project.SpringBootThymeLeafCrudWebApp.model.Employee;
+import com.project.SpringBootThymeLeafCrudWebApp.models.Employee;
 import com.project.SpringBootThymeLeafCrudWebApp.repository.EmployeeRepository;
 import com.project.SpringBootThymeLeafCrudWebApp.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
@@ -15,5 +17,10 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        employeeRepository.save(employee);
     }
 }
