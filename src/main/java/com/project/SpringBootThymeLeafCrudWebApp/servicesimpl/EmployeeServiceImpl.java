4fb487c18686f853da4e627793f110ffa0c665c3
david-kariuki <davidkariukikamau0@gmail.com>
2022-04-19 16:53:46 +0300

    Created methods to get employee by id and delete employee

--- src/main/java/com/project/SpringBootThymeLeafCrudWebApp/servicesimpl/EmployeeServiceImpl.java
+++ src/main/java/com/project/SpringBootThymeLeafCrudWebApp/servicesimpl/EmployeeServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -22,5 +23,32 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public void saveEmployee(Employee employee) {
         employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(Long employeeId) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(employeeId);
+        Employee employee = null;
+        if (optionalEmployee.isPresent()) {
+            employee = optionalEmployee.get();
+        } else {
+            throw new RuntimeException("Employee not found for id : " + employeeId + "!!");
+        }
+        return employee;
+    }
+
+    @Override
+    public boolean deleteEmployeeById(Long employeeId) {
+
+        Optional<Employee> optionalEmployee = employeeRepository.findById(employeeId);
+
+        if (optionalEmployee.isPresent()) {
+            Employee employee = optionalEmployee.get();
+            employeeRepository.delete(employee);
+
+            return true;
+        } else {
+            throw new RuntimeException("Employee not found for id : " + employeeId + "!!");
+        }
     }
 }
